8234ab5965deff65a8b8b4bbd61bc60fe5b0bb60
Luis <luismartinezmilla@gmail.com>
2021-11-22 14:19:40 +0100

    yes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\CursosController;
+use App\Http\Controllers\VideosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('usuario')->group(function(){
+    Route::get('/registrar', [UsuariosController::class,'registrar']);
+    Route::delete('/desactivar', [UsuariosController::class,'desactivar']);
 });
+Route::prefix('curso')->group(function(){
+    Route::put('/crear', [CursosController::class,'crear']);
+    Route::put('/editar', [CursosController::class,'editar']); //aqui se le añaden los videos
+    Route::delete('/borrar', [CursosController::class,'borrar']);
+});
+// Route::prefix('cursos')->group(function(){
+//     Route::put('/crear', [CursosController::class,'crear']);
+//     Route::delete('/borrar', [CursosController::class,'crear']);
+// });
